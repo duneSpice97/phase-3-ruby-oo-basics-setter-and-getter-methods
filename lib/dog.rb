6d36ddf09e4db673c0e756be02524ccd36c6a88a
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-14 18:23:05 -0700

    commit tree 0b9b3a8babdef949639235827f44cb1e8ea7aad3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
+    # define setter method
+    def name=(name)
+        @name = name
+    end
+
+    # define getter method
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
